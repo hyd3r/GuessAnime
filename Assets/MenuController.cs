6a408e4d321791326c366331b2ad188a0bc39ff1
hyd3r <hyd3r.exe@gmail.com>
2021-05-25 12:41:52 +0800

    init

---
+++ Assets/MenuController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class MenuController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject usernameMenu;
+    [SerializeField] private GameObject connectPanel;
+
+    [SerializeField] private TMP_InputField usernameInput;
+    [SerializeField] private InputField createGameInput;
+    [SerializeField] private InputField joinGameInput;
+
+    [SerializeField] private GameObject startButton;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Start()
+    {
+        usernameMenu.SetActive(true);
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+
+    public void ChangedUserNameInput()
+    {
+        if(usernameInput.text.Length >= 3)
+        {
+            startButton.SetActive(true);
+        }
+        else
+        {
+            startButton.SetActive(false);
+        }
+    }
+
+    public void SetUserName()
+    {
+        usernameMenu.SetActive(false);
+        PhotonNetwork.NickName = usernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom(createGameInput.text, new RoomOptions() { MaxPlayers = 4 }, null);
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 5;
+        PhotonNetwork.JoinOrCreateRoom(joinGameInput.text, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MainGame");
+    }
+}
---
+++ Assets/RoomController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomController : MonoBehaviour
+{
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
